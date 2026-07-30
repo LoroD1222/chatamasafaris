01b989109d9d35f2d224c33719f8190a1d4369ef
Lovro Krizan <lovro.webdesign@gmail.com>
2026-07-30 22:06:27 +0300

    Add mobile image strip asset

--- src/features/home/home-page.tsx
+++ src/features/home/home-page.tsx
@@ -423,7 +423,8 @@ function ImageStrip() {
   return (
     <section aria-label="Tanzania landscape gallery" className="relative -mt-[100px] bg-astra-cream">
       <div className="relative h-[230px] w-full overflow-hidden md:h-[322px] lg:h-[404px]">
-        <Image src="/assets/figma/choose-tanzania-experience.png" alt="" fill sizes="(min-width: 768px) 100vw, 140vw" className="scale-[1.4] object-cover object-right md:scale-100 md:object-center" />
+        <Image src="/assets/figma/image-strip-mobile-mountain.png" alt="" fill sizes="100vw" className="object-cover object-center md:hidden" />
+        <Image src="/assets/figma/choose-tanzania-experience.png" alt="" fill sizes="100vw" className="hidden object-cover object-center md:block" />
       </div>
     </section>
   );
